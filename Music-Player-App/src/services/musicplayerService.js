e7fcbb112c624427172fe9caf3fcff5f60c1a0eb
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2025-01-12 21:13:14 +0200

    updated

--- Music-Player-App/src/services/musicplayerService.js
+++ Music-Player-App/src/services/musicplayerService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const API_BASE_URL = "https://api.example.com";
+const API_BASE_URL = "https://via.placeholder.com/300";
 export const fetchData = async (endpoint) => {
     try {
-        const response = await axios.get('${API_BASE_URL}/${endpoint}');
+        const response = await fetch("/mock-playlist.json");
         return response.data;
     } catch ( error ) { 
         console.error("Error fetching data:", error);
