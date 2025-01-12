5f097549f8afdf2eb54a2aa9e1a19e83aa7d5fc3
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2025-01-12 20:29:31 +0200

    updated

--- Music-Player-App/eslint.config.js
+++ Music-Player-App/eslint.config.js
@@ -1,42 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig } from 'vite'
-import reacr from "@vitejs/plugin-react"
-
-export default defineConfig ({ 
-
-  ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "jsx-a11y"],
+  rules: {
+    "react/prop-types": 0, // Disable prop-types rule (optional for TypeScript)
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "jsx-a11y/no-autofocus": 0, // Disable autofocus warning
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
   },
-)
-
+};
