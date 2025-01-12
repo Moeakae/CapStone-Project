1ee02b9d5dabdb6abe9d23a12f422979253d1de4
Lisa-pnap <ntsoakilekhoakhoa@gmail.com>
2025-01-12 20:29:54 +0200

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // Import TailwindCSS styles
 
-import './index.css'; // Make sure the file exists or adjust the path
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
 );
 
